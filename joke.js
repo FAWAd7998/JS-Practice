5dd43377e6987b097bc99e68e1f7e6127fb817f4
FAWAd7998 <fawad7998@gmail.com>
2023-01-05 22:19:30 +0500

    Initial Commit

---
+++ joke.js
@@ -0,0 +1,239 @@
+// // <!-- <script>
+// // //for of
+// // let d = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// // for (let c of d) {
+// // 	//  console.log(d)
+// // }
+
+// // // 1
+// // let b = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+
+// // // console.log(b[0]);
+// // // console.log(b[8]);
+
+// // // 2
+// // let a = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+
+// // // console.log(a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8]);
+// // // console.log(a[0+1+2+3+4+5+6+7+8])
+
+// // //SUM THE VALUES OF AN ARRAY
+
+// // let abc = [33, 43, 54354, 766767, 545, 43434];
+
+// // let m = abc.reduce((b) => {
+// // 	return b;
+// // });
+// // // console.log(m);
+// // let e = [h, e, l, l, o];
+
+// // e.forEach((abcd) => {
+// // 	console.log(e[abcd]);
+// // });
+// // </script> -->
+
+let a = [
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+	"What did one toilet say to the other You look a bit flushed",
+
+	" What do you think of that new diner on the moon Food was good, but there really wasnt much atmosphere.",
+
+	" Why did the dinosaur cross the road  Because the chicken wasnt born yet.",
+
+	" Why cant Elsa from Frozen have a balloon  Because she will let it go, let it go. ",
+
+	" What musical instrument is found in the bathroom? A tuba toothpaste.",
+
+	" Related: Funny Christian Jokes",
+
+	"Why did the kid bring a ladder to school? Because she wanted to go to high school.",
+
+	"  What do you call a dog magician? A labracadabrador.",
+
+	" Where would you find an elephan The same place you lost her.",
+];
+
+
+
+
+
+// method no 01
+
+// let element = document.getElementsByClassName('abcd')[0];
+// element.innerText = a[Math.floor(Math.random(1, 101)*9)]
+// console.log()
+
+
+
+
+
+
+
+
+
+
+
+// method no 02
+
+// let b = Math.floor(Math.random() * a.length);
+// // console.log(b);
+// abcde.innerHTML = a[b];
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // without making variable
+// let c= document.getElementsByClassName("abcd")[0];
+// let c = abcde.innerHTML = a[Math.floor(Math.random() * a.length)];
+
+// document.getElementById(btn)
+// // btn.addEventlistner('click')
+// // console.log(c);
+// abcde.innerHTML = a[Math.floor(Math.random() * a.length)];
+
+
+
+
+
+
+
+
+// sorting function
+
+
+let n=[234,43,4,5665,6656,4545,2332,2323,34,345,7898989,6,7,3,1]
+
+let d= n.sort(
+    (a,b)=>{
+   return b-a;
+    }
+  );
+
+console.log(d)
+
+// btn.addEventListner('click', )
---
+++ latest .js
@@ -0,0 +1,29 @@
+let a = [
+	12, 12, 1, 233, 22, 3, 5, 23, 23, 323, 23, 545,"sdfdsfsdfsdfsffdsdsfsffsfsd", 45, 67, 676, 7787878, 78787,
+	988989, 89899,
+];
+
+// for loop
+for (let n = 0; n < a.length; n++) {
+    //   console.log(	a[n]);
+}
+
+
+
+
+// for in loop 
+for (let b in a) {
+	// console.log(a[b])
+}
+
+
+// for of loop
+
+for (let b of a) {
+	// console.log(b)
+}
+
+a.forEach(b => {
+	console.log(b)
+	
+});
